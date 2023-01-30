a584cdede141c9b90c14a6a83244f8411b08cb6b
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-01-30 20:02:13 +0700

    disable save model and artifact

---
+++ src/models/convnext_module.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import wandb
+from components.convnext import convnext_base
+from torchmetrics import Accuracy
+
+
+class ConvNeXtLitModel(pl.LightningModule):
+    def __init__(self, input_shape, num_classes, learning_rate=1e-3, transfer=False, model_name="convnext_base"):
+        super().__init__()
+        
+        # log hyperparameters
+        self.save_hyperparameters()
+        self.learning_rate = learning_rate
+        self.dim = input_shape
+        self.num_classes = num_classes
+        self.model_name = model_name
+        
+        # transfer learning if pretrained=True
+        self.feature_extractor = convnext_base(pretrained=transfer)
+
+        if transfer:
+            # layers are frozen by using eval()
+            self.feature_extractor.eval()
+            # freeze params
+            for param in self.feature_extractor.parameters():
+                param.requires_grad = False
+        
+        n_sizes = self._get_conv_output(input_shape)
+
+        self.classifier = nn.Linear(n_sizes, num_classes)
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
+  
+    # returns the size of the output tensor going into the Linear layer from the conv block.
+    def _get_conv_output(self, shape):
+        batch_size = 1
+        tmp_input = torch.autograd.Variable(torch.rand(batch_size, *shape))
+
+        output_feat = self._forward_features(tmp_input) 
+        n_size = output_feat.data.view(batch_size, -1).size(1)
+        return n_size
+        
+    # returns the feature tensor from the conv block
+    def _forward_features(self, x):
+        x = self.feature_extractor(x)
+        return x
+    
+    # will be used during inference
+    def forward(self, x):
+       x = self._forward_features(x)
+       x = x.view(x.size(0), -1)
+       x = self.classifier(x)
+       
+       return x
+    
+    def training_step(self, batch):
+        batch, gt = batch[0], batch[1]
+        out = self.forward(batch)
+        loss = self.criterion(out, gt)
+
+        acc = self.accuracy(out, gt)
+
+        self.log("train/loss", loss)
+        self.log("train/acc", acc)
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        batch, gt = batch[0], batch[1]
+        out = self.forward(batch)
+        loss = self.criterion(out, gt)
+
+        self.log("val/loss", loss)
+
+        acc = self.accuracy(out, gt)
+        self.log("val/acc", acc)
+
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        batch, gt = batch[0], batch[1]
+        out = self.forward(batch)
+        loss = self.criterion(out, gt)
+        
+        return {"loss": loss, "outputs": out, "gt": gt}
+    
+    def test_epoch_end(self, outputs):
+        loss = torch.stack([x['loss'] for x in outputs]).mean()
+        output = torch.cat([x['outputs'] for x in outputs], dim=0)
+        
+        gts = torch.cat([x['gt'] for x in outputs], dim=0)
+
+        # torchmodel = "model_final.pth"
+        # torch.save(model.state_dict(), torchmodel)
+        # artifact = wandb.Artifact(name=f"model_{self.model_name}.ckpt", type="model")
+        # artifact.add_file(torchmodel)
+        # wandb.log_artifact(artifact)
+        
+        self.log("test/loss", loss)
+        acc = self.accuracy(output, gts)
+        self.log("test/acc", acc)
+        
+        self.test_gts = gts
+        self.test_output = output
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
