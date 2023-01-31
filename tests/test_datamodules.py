2dee14d598f62444ae0649cd3bec4566616068f4
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-01-31 11:10:17 +0700

    update requirements.txt and test datamodule

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -3,19 +3,20 @@ from pathlib import Path
 import pytest
 import torch
 
-from src.data.mnist_datamodule import MNISTDataModule
+from src.data.covid19_datamodule import Covid19DataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
-def test_mnist_datamodule(batch_size):
+def test_covid19_datamodule(batch_size):
     data_dir = "data/"
 
-    dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
+    dm = Covid19DataModule(data_dir=data_dir, batch_size=batch_size)
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_val and not dm.data_test
-    assert Path(data_dir, "MNIST").exists()
-    assert Path(data_dir, "MNIST", "raw").exists()
+    assert Path(data_dir, "Covid19-dataset").exists()
+    assert Path(data_dir, "Covid19-dataset", "train").exists()
+    assert Path(data_dir, "Covid19-dataset", "test").exists()
 
     dm.setup()
     assert dm.data_train and dm.data_val and dm.data_test
