9c40d5baa8a36fda2d4862a446f078cc641ed7b1
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-01-31 11:14:50 +0700

    modify setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
+    description="COVID-19 Recognition",
     author="",
     author_email="",
-    url="https://github.com/user/project",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url="https://github.com/adhiiisetiawan/covid19-recognition",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     install_requires=["pytorch-lightning", "hydra-core"],
     packages=find_packages(),
 )
